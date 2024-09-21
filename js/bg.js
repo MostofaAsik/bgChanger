fcbced1cea9f61b4bfe9307fe1d26ad003bd7b6b
MostofaAsik <mmostofaasik@gmail.com>
2024-09-21 12:43:09 +0600

    way-2 created2way

--- js/bg.js
+++ js/bg.js
@@ -4,10 +4,32 @@ const body = document.querySelector('body')
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         const id = e.target.id
-        if (id === 'red' || id === 'grey' || id === 'yellow' || id === 'tomato' || id === 'white' || id === 'black') {
+
+        //way-2.1 >>>myWAy
+        // if (id === 'red' || id === 'grey' || id === 'yellow' || id === 'tomato' || id === 'white' || id === 'black') {
+        //     body.style.backgroundColor = id
+        // }
+
+        // way-2.2
+        if (id === 'red') {
             body.style.backgroundColor = id
         }
+        if (id === 'grey') {
+            body.style.backgroundColor = id
+        }
+        if (id === 'yellow') {
+            body.style.backgroundColor = id
+        }
+        if (id === 'tomato') {
+            body.style.backgroundColor = id
+        }
+        if (id === 'white') {
+            body.style.backgroundColor = id
+        }
+        if (id === 'black') {
+            body.style.backgroundColor = id
+        }
+
+
     })
 })
-
-console.log(buttons);
